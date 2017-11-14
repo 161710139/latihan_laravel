8292abbb68a66f483c765f16da7ebb6b595aa791
161710139 <161710139@smkassalaambandung.sch.id>
2017-11-14 10:02:57 +0700

    Laravel Haikal

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,22 @@
 <?php
+//basic routing
+Route::get('/'.function () {
+    return view('Welcome');
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/about',function (){
+    return '<h1> Hallo </h1>'
+            .'Selamat Datang di Web App saya<br>'
+            .'Laravel Keren'
+});
 
-Route::get('/', function () {
-    return view('welcome');
+//routing parameters
+Route::get('user/{nama}',function ($a) {
+    return 'Nama saya adalah <b>'.$a.'</b>';
 });
-Route::get('/about', function () {
-    return 'Rafy Ganteng';
-});
-Route::get('/kantin/{makanan}/{minuman}/{cemilan}',function($a,$b,$c){
- return 'Makan '.$a.'<br>'.
- 		'Minum '.$b.'<br>'.
- 		'Ngemil '.$c.'<br>';
-});
-Route::get('user/{nama?}',function($a= "Gareth Bale")
-return $a;
-});
+
+//tugas routing
+Route::get('/kantin/{makanan}/{minuman}/{cemilan}',function($makan,$minum,$cemil){
+    return 'Saya Makan '.$makan.'<br>'.'Minumnya '.$minum.'<br>'.'Ngemilnya'.$ngemil.'<br>';
+})
+?>
